4f65b549e10b2ecf1d2475c08e59b881a0bf7b07
Donal O Sullivan <osulld13@github.com>
2014-11-26 11:19:37 +0000

    Commit

--- assignment2.pl
+++ assignment2.pl
@@ -6,52 +6,18 @@
 	over the alphabet {0, 1} such that the number of 0’s in u is the same as the
 	number of 1’s in v.*/
 
-	/*s --> u, [2], v.
+	s --> u(X), [2], v(Y), {X =:= Y}.
 
-	u --> digU.
+	u(X) --> [1], u(Z), {X is Z}.
+	u(X) --> [0], u(Z), {X is (Z + 1)}.
+	u(0) --> [].
 
-	v --> digV.
+	v(Y) --> [0], v(Z), {Y is Z}.
+	v(Y) --> [1], v(Z), {Y is (Z + 1)}.
+	v(0) --> [].
 
-	digU --> [].
-	digU --> [0], digU.
-	digU --> [1], digU.
+	part1(X) :- s(X, []).
 
-	digV --> [].
-	digV --> [0], digV.
-	digV --> [1], digV.
+% part 2
 
-	part1(X) :- s(X, []).*/
-
-	% with counts
-
-	/*s(Accum1) --> u(Count1, Accum1), [2], v.
-
-	u(Count1, Accum1) --> digU(Count1, Accum1).
-
-	v --> digV.
-
-	digU(Count1, Accum1) --> [1], digU(Var, Accum1), Count1 is Var.
-	digU(Count1, Accum1) --> [0], digU(Var, Accum1), Count1 is (Var + 1).
-	digU(Count1, Count1) --> [].
-
-	digV --> [].
-	digV --> [0], digV.
-	digV --> [1], digV.
-
-	part1(X, Y) :- s(X, Y, []).*/
-
-	s(Count1) --> u(Count1), [2], v.
-
-	u(Count1) --> digU(Count1).
-
-	v --> digV.
-
-	digU(Count1) --> [1], digU(Var), Count1 is Var.
-	digU(Count1) --> [0], digU(Var), Count1 is (Var + 1).
-	digU(0) --> [].
-
-	digV --> [].
-	digV --> [0], digV.
-	digV --> [1], digV.
-
-	part1(X, Y) :- s(X, Y, []).
+	/**/
