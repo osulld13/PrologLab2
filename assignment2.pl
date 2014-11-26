59f495f3156ced5ac2d9bbadf1b8326f717893bf
Donal O Sullivan <osulld13@github.com>
2014-11-26 14:31:19 +0000

    Commit

--- assignment2.pl
+++ assignment2.pl
@@ -1,18 +1,14 @@
 %Assignment 2
 
 % part 1
-	
-	/*Write a DCG that accepts strings of the form u2v where u and v are strings
-	over the alphabet {0, 1} such that the number of 0’s in u is the same as the
-	number of 1’s in v.*/
 
 	s --> u(X), [2], v(Y), {X =:= Y}.
 
-	u(X) --> [1], u(Z), {X is Z}.
+	u(X) --> [1], u(X).
 	u(X) --> [0], u(Z), {X is (Z + 1)}.
 	u(0) --> [].
 
-	v(Y) --> [0], v(Z), {Y is Z}.
+	v(Y) --> [0], v(Y).
 	v(Y) --> [1], v(Z), {Y is (Z + 1)}.
 	v(0) --> [].
 
@@ -20,4 +16,18 @@
 
 % part 2
 
-	/**/
+	street(X) --> col(X, Y) , nat, pet, col(Y, Z), nat, pet, col(Z, _), nat, pet.
+
+	col(X, Y) --> {not(member(red, X))}, [red], {append([red], X, Y)}.
+	col --> [blue].
+	col --> [green].
+
+	nat --> [english].
+	nat --> [spanish].
+	nat --> [japanese].
+
+	pet --> [jaguar].
+	pet --> [snail].
+	pet --> [zebra].
+
+	part2(S) :- street([], S, []).
