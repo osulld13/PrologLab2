1fefba4254d386c3ff85e7daf0b13826e4c0b2cf
Donal O Sullivan <osulld13@github.com>
2014-11-26 09:10:01 +0000

    Commit

--- Asssignment2.pl
+++ Asssignment2.pl
@@ -6,4 +6,15 @@
 	%				Has only 0s and 1s
 	% Satisfies condition (Number of 1s = Number of 0s)
 
-	s --> u, 2, v.
+	s --> u, [2], v.
+
+	u --> dig, u.
+	u --> dig.
+
+	v --> dig, v.
+	v --> dig.
+
+	dig --> [0].
+	dig --> [1].
+
+	part1(X) :- s(X, []).
